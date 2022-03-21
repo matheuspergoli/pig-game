6b25d6f071514c0a90757d7b9b406223a40a31ef
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-21 17:53:31 -0300

    Selecionando elementos e condições iniciais

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecionando elementos
+const diceElement = document.querySelector('.dice')
+const score0Element = document.querySelector('#score--0')
+const score1Element = document.querySelector('#score--1')
+
+// Condições iniciais
+score0Element.textContent = 0
+score1Element.textContent = 0
+diceElement.classList.add('hidden')
