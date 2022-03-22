4e4bf6c65720636aa1bec5eb0f9015486717a953
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-22 04:46:08 -0300

    Concluindo as funcionalidades
    Terminando as funcionalidades de pontuação, win, troca de player e estado do jogo

--- script.js
+++ script.js
@@ -24,28 +24,55 @@ diceElement.classList.add('hidden')
 const scores = [0, 0]
 let currentScore = 0
 let activePlayer = 0
+let playing = true
+
+const switchPlayer = () => {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0Element.classList.toggle('player--active')
+    player1Element.classList.toggle('player--active')
+}
 
 // Funcionalidade roll do dado
 btnRoll.addEventListener('click', () => {
-    // 1. Gerar um roll aleatório 
-    const dice = Math.trunc(Math.random() * (6 - 0) + 1)
+    if (playing) {
+        // 1. Gerar um roll aleatório 
+        const dice = Math.trunc(Math.random() * (6 - 0) + 1)
+    
+        // 2. Mostrar o dado
+        diceElement.classList.remove('hidden')
+        diceElement.src = `dice-${dice}.png`
+    
+        // 3. Checar se o roll é 1
+        if (dice !== 1) {
+            // Adicionar o roll ao current score
+            currentScore += dice
+            document.querySelector(`#current--${activePlayer}`).textContent = currentScore
+    
+        } else {
+            // Trocar de player
+            switchPlayer()
+        }
+    }
+})
 
-    // 2. Mostrar o dado
-    diceElement.classList.remove('hidden')
-    diceElement.src = `dice-${dice}.png`
-
-    // 3. Checar se o roll é 1
-    if (dice !== 1) {
-        // Adicionar o roll ao current score
-        currentScore += dice
-        document.querySelector(`#current--${activePlayer}`).textContent = currentScore
-
-    } else {
-        // Trocar de player
-        document.querySelector(`#current--${activePlayer}`).textContent = 0
-        activePlayer = activePlayer === 0 ? 1 : 0
-        currentScore = 0
-        player0Element.classList.toggle('player--active')
-        player1Element.classList.toggle('player--active')
+btnHold.addEventListener('click', () => {
+    if (playing) {
+        // 1. Adicionar pontuação atual para a pontuação do player ativo
+        scores[activePlayer] += currentScore
+        document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer]
+    
+        // 2. Checar se o score do player é maior ou igual a 100
+        if (scores[activePlayer] >= 20) {
+            // Jogo termina
+            playing = false
+            diceElement.classList.add('hidden')
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+        } else {
+            // Muda para o próximo player
+            switchPlayer()
+        }
     }
 })
